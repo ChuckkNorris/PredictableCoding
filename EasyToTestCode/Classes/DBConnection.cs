658509b93654f93e6d29ccdc15e290d85bee7bc0
CREDERA\ccaicedo <ccaicedo@credera.com>
2017-09-21 15:48:32 -0500

    Moving EasyToTest to it's own project

---
+++ EasyToTestCode/Classes/DBConnection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PredictableCoding.Testing
+{
+    public class DBConnection
+    {
+        public DBConnection(string str);
+        public void Connect();
+        public Model Search() { return new Model(); }
+    }
+}
