8f1ec51ff62bc3c42a0708fcc69b57fb23087fa1
CREDERA\ccaicedo <ccaicedo@credera.com>
2017-10-03 20:33:36 -0500

    Last edits for demo

--- src/EasyToTestCode_UnitTest/TestSearch.cs
+++ src/EasyToTestCode_UnitTest/TestSearch.cs
@@ -12,24 +12,24 @@ namespace EasyToTestCode_UnitTest
         [TestMethod]
         public void BadSearchKeyword()
         {
-            //string Keyword = "round";
-            //List<string> Words = { "round", "square", "cylindrical" };
-            //var searchRequest = new SearchRequest(Keyword, Words);
-            //... code
-            //var result = ctx.OKSearch(searchRequest);
-            //Assert.IsTrue(result.value > 0);
+            string Keyword = "round";
+            List<string> Words = { "round", "square", "cylindrical" };
+            var searchRequest = new SearchRequest(Keyword, Words);
+            ///... code
+            var result = ctx.OKSearch(searchRequest);
+            Assert.IsTrue(result.value > 0);
         }
 
         [TestMethod]
         public void BetterSearchKeyword()
         {
-            //string Keyword = "round";
-            //List<string> Words = { "round", "square", "cylindrical" };
-            //var searchRequest = new SearchRequest(Keyword, Words);
-            //var searchRequest = new SearchRequest();
-            //... code
-            //var result = ctx.OKSearch(searchRequest);
-            //Assert.IsTrue(result.length == 1 && result.value == Keyword);
+            string Keyword = "round";
+            List<string> Words = { "round", "square", "cylindrical" };
+            var searchRequest = new SearchRequest(Keyword, Words);
+            var searchRequest = new SearchRequest();
+            ///... code
+            var result = ctx.OKSearch(searchRequest);
+            Assert.IsTrue(result.length == 1 && result.value == Keyword);
         }
     }
 }
