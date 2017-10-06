358a03bcbbb37258f7ddf0d2518c79ae3b1c56d4
Scott B <sablank725@gmail.com>
2017-10-05 23:35:30 -0500

    Updated StyleCop documentation
    Finalized the StyleCop documentation and added a few examples in the StyleCop project to use for the demo.

---
+++ src/StyleCop_Enforcer/ApiController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NamingIsHard_Code.IntentfulNamingDemo
+{
+    public abstract class ApiController
+    {
+        public object Request { get; set; }
+    }
+
+    internal class HttpGetAttribute : Attribute
+    {
+    }
+}
